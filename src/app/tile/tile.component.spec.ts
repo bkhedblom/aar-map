7e04f6a4b58adad345632f70c515f9b08fb8486f
Björn Hedblom <git@singali.net>
2018-11-20 23:41:29 +0100

    Refactors tile component and spec

--- src/app/tile/tile.component.spec.ts
+++ src/app/tile/tile.component.spec.ts
@@ -1,12 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TileComponent } from './tile.component';
-import { Tile } from 'src/app/tile/tile.model';
+import { Tile } from './tile.model';
 
 describe('TileComponent', () => {
   let component: TileComponent;
   let fixture: ComponentFixture<TileComponent>;
   let compiledHtml: HTMLElement;
+  let input:Tile;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ TileComponent ]
@@ -14,6 +16,8 @@ describe('TileComponent', () => {
     fixture = TestBed.createComponent(TileComponent);
     component = fixture.componentInstance;
     compiledHtml = fixture.debugElement.nativeElement;
+    input = new Tile("This is a test tile");
+    component.tile = input;
     fixture.detectChanges();
   });
 
@@ -31,37 +35,32 @@ describe('TileComponent', () => {
   })
 
   it('should display the tile text if tile is face up', () => {
-    const input = new Tile("This is a test tile");
     input.isFaceUp = true;
-    component.tile = input;
     fixture.detectChanges();
     expect(component.textToDisplay).toEqual(input.text);
   })
 
   it("should not display text if tile is face down", () => {
-    const input = new Tile("This is a test tile");
     input.isFaceUp = false;
-    component.tile = input;
     fixture.detectChanges();
     expect(component.textToDisplay).toEqual("");
   })
 
   it('should give tile div class face-down if face down', () => {
-    component.tilefaceDown = true;
+    input.isFaceUp = false;
     fixture.detectChanges();
     expect(compiledHtml.querySelector('div.tile').classList).toContain("face-down");
   })
 
-  it('should give tile div class face-up if not face down', () => {
-    component.tilefaceDown = false;
+  it('should give tile div class face-up if face up', () => {
+    input.isFaceUp = true;
     fixture.detectChanges();
     expect(compiledHtml.querySelector('div.tile').classList).toContain("face-up");
   })
 
-  it('should flip face when clicked', () => {
-    component.tilefaceDown = true;
-    fixture.detectChanges();
+  it('should flip tile face when clicked', () => {
+    input.isFaceUp = false;
     compiledHtml.querySelector('div.tile').dispatchEvent(new Event("click"));
-    expect(component.tilefaceDown).toBeFalsy();
+    expect(input.isFaceUp).toBe(true);
   })
 });
